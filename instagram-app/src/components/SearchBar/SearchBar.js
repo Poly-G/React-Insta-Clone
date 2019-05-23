f1a702b611addaa7a380314edb07c44f501559db
Poly Goveia <poly.goveia@gmail.com>
2019-05-22 19:52:48 -0700

    add header content

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import './search.css';
 
 class SearchBar extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="search">
+                <p>
+                    <i class="fab fa-instagram"></i>
+                    <span>|</span>
+                    <span>Instagram</span>
+                </p>
+                <input/>
+                <p>
+                    <i class="far fa-compass"></i>
+                    <i class="far fa-heart"></i>
+                    <i class="far fa-user"></i>
+                </p>
             </div>
         )
     }
