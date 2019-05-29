2d599948690bf32466f360d03709dd76171cf7f5
Poly Goveia <poly.goveia@gmail.com>
2019-05-28 19:04:41 -0700

    add thumbnail and username

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import CommentSection from '../CommentSection/CommentSection';
 
 class PostContainer extends Component {
     render() {
+        
         return (
             <div>
-                
+                {this.props.data.map(post => {
+                    return (
+                        <div className="header">
+                            <img
+                            className="thumbnail"
+                            src={post.thumbnailUrl}
+                            alt="thumbnail"
+                            />
+                            <p className="username">{post.username}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
